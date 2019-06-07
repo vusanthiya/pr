7962e07a6c3d49869721af493ce063f1cc3d63ac
vusanthiya <vusanthiya@gmail.com>
2019-06-07 14:33:23 +0530

    Update day.py

--- day.py
+++ day.py
@@ -1,5 +1,5 @@
 st=str(input())
-if (st=="monday" or st=="tuesday" or st=="wednesday" or st=="thursday" st=="friday"):
+if (st=="monday" or st=="tuesday" or st=="wednesday" or st=="thursday" st=="friday" or st=="saturday"):
  print ("no")
 else:
  print ("yes")
